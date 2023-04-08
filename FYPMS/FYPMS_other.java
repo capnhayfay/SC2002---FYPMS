1330fe91a00cc44f8b02244dc593e77d8c0b5874
capnhayfay <singh.dhruv2000@gmail.com>
2023-04-08 15:51:09 +0800

    Commit FYPMS_other

---
+++ FYPMS/FYPMS_other.java
@@ -0,0 +1,30 @@
+package FYPMS;
+
+import moblima.booking.BookingHistory;
+import moblima.cineplex.CineplexList;
+import moblima.movie.MovieList;
+
+/**
+ * Represents the company this application is created for
+ * Contains all bookings, movies and cineplexes the company owns
+ */
+public class FYPMS_other {
+
+    private static final ProjectHistory projectHistory = new BookingHistory();
+
+    private static final MovieList movieList = new MovieList();
+
+    private static final CineplexList cineplexList = new CineplexList();
+
+    public static BookingHistory getBookingHistory() {
+        return bookingHistory;
+    }
+    public static MovieList getMovieList() {
+
+        return movieList;
+    }
+    public static CineplexList getCineplexList() {
+        return cineplexList;
+    }
+
+}

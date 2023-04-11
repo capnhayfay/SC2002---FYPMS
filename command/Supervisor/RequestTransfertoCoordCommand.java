f4b65d1b067cb14cd7e19101c29e0d5b34d3d8d5
Zhiqi <guozhiqi1999@gmail.com>
2023-04-12 04:26:22 +0800

    change

--- command/Supervisor/RequestTransfertoCoordCommand.java
+++ command/Supervisor/RequestTransfertoCoordCommand.java
@@ -1,8 +1,24 @@
 package command.Supervisor;
 
+import java.time.LocalDateTime;
 import command.Command;
+import FYPMS.FYPMS;
+import FYPMS.project.*;
+import FYPMS.faculty.supervisor.*;
+import FYPMS.request.*;
 
 public class RequestTransfertoCoordCommand implements Command {
+    private String requester;
+    
+    public RequestTransfertoCoordCommand(String requester){
+        this.requester = requester;
+    }
+    RequestList RequestList = FYPMS.getRequestList();
+
     public void execute() {
+        LocalDateTime statusChangeTime = LocalDateTime.now();
+        Request request = new Request(requester, RequestType.SUPERVISORCoordinator, statusChangeTime,
+                "FYP Coordinator", RequestStatus.PENDING);
+        RequestList.add(request);
     }
 }

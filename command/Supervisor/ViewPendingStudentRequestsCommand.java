090ee29fdc59f195ded6f77fa9838a6ddbdc0d88
Zhiqi <guozhiqi1999@gmail.com>
2023-04-12 04:39:34 +0800

    change

--- command/Supervisor/ViewPendingStudentRequestsCommand.java
+++ command/Supervisor/ViewPendingStudentRequestsCommand.java
@@ -1,8 +1,32 @@
 package command.Supervisor;
 
 import command.Command;
+import java.util.ArrayList;
+
+import FYPMS.request.*;
 
 public class ViewPendingStudentRequestsCommand implements Command {
+    private String Supervisor;
+    private RequestList requests;
+
+    public ViewPendingStudentRequestsCommand(String Supervisor, RequestList requests){
+        this.Supervisor = Supervisor;
+        this.requests = requests;
+    }
+
     public void execute() {
+        int RequestCount = 1;
+        RequestList requests = FYPMS.getRequestList();
+        System.out.println();
+        System.out.println("Pending Request");
+        System.out.println();
+        for (Request request : requests) {
+            if () {
+                System.out.println("============= Request No. " + RequestCount++ + " ==============");
+                request.printDetails();
+                System.out.println();
+            }
+        }
+
     }
 }
--- command/Supervisor/ViewSupervisorProjectStatusCommand.java
+++ command/Supervisor/ViewSupervisorProjectStatusCommand.java
@@ -1,8 +1,33 @@
 package command.Supervisor;
 
+import java.util.ArrayList;
+
+import FYPMS.project.FYP;
+import FYPMS.project.FYPList;
 import command.Command;
 
 public class ViewSupervisorProjectStatusCommand implements Command {
+    private String Supervisor;
+    private FYPList projects;
+
+    public ViewSupervisorProjectStatusCommand(String Supervisor, FYPList projects) {
+        this.Supervisor = Supervisor;
+        this.projects = projects;
+    }
+
     public void execute() {
+        int fypCount = 1;
+        ArrayList<FYP> fyps = projects.getFYPs();
+        System.out.println();
+        System.out.println("List of all Final Year Projects");
+        System.out.println();
+        for (FYP filteredFyp : fyps) {
+            if (filteredFyp.getSupervisorName().equals(Supervisor)) {
+                System.out.println("============= FYP No. " + fypCount++ + " ==============");
+                filteredFyp.printFYPDetails();
+                System.out.println();
+            }
+        }
+        System.out.println("===== There are " + (fypCount - 1) + " Final Year Projects! =====");
     }
 }

e4f4dca64a54f1408229388cf973f61907b7b5c1
Guo Zhiqi <zq@Zqs-MacBook-Air.local>
2023-04-12 03:59:35 +0800

    change

--- command/Supervisor/ModifySubmittedFYPTitleCommand.java
+++ command/Supervisor/ModifySubmittedFYPTitleCommand.java
@@ -1,8 +1,21 @@
 package command.Supervisor;
 
 import command.Command;
+import FYPMS.FYPMS;
+import FYPMS.project.*;
 
 public class ModifySubmittedFYPTitleCommand implements Command {
+    private FYP project;
+    private String newtitle;
+
+    public ModifySubmittedFYPTitleCommand(FYP project, String newtitle){
+        this.project = project;
+        this.newtitle = newtitle;
+    }
+
     public void execute() {
+        System.out.println();
+        project.setTitle(newtitle);
+        System.out.println("Title has been change to : "+ newtitle);
     }
 }

27378c0568b90d01c46b19af43fc5343bb85b433
Zhiqi <guozhiqi1999@gmail.com>
2023-04-16 01:32:02 +0800

    Co-authored-by: Zoey Lam <Zomankey@users.noreply.github.com> Co-authored-by: Jovin2525 <Jovin2525@users.noreply.github.com> Co-authored-by: capnhayfay <capnhayfay@users.noreply.github.com> Co-authored-by: Bryan <zonpig@users.noreply.github.com>

--- FYPMS/request/RequestRegister.java
+++ FYPMS/request/RequestRegister.java
@@ -8,13 +8,4 @@ public class RequestRegister extends Request {
         super(requestID, requesterID, AccountManager.getCoordinatorList().get(0).getLoginId(), requestStatus, RequestRelationship.STUDENTCoordinator, RequestType.REGISTER_PROJECT, fypID);
     }
 
-    public void printDetails() {
-        System.out.println("Requester: " + getRequesterID());
-        System.out.println("Requestee: " + getRequesteeID());
-        System.out.println("Request Type: " + getRequestType() );
-        System.out.println("Request Status: " + getRequestStatus());
-        System.out.println("Request Relationship: " + getRequestRelationship());
-        System.out.println("FYP ID: " + getFypID());
-        System.out.println();
-    }
 }
